a7e3193b392199a3f807b6bb44f5c29fbe08793c
Dishant <agrawaldishant29@gmail.com>
2022-09-05 11:40:11 +0530

    added admin_user_provider.dart to store logged in user data

---
+++ lib/providers/admin_user_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+
+import '../models/admin_user_model.dart';
+
+class AdminUserProvider extends ChangeNotifier {
+  AdminUserModel? _adminUserModel;
+
+  AdminUserModel? getAdminUserModel() {
+    if(_adminUserModel != null) {
+      return AdminUserModel.fromMap(_adminUserModel!.toMap());
+    }
+    else {
+      return null;
+    }
+  }
+
+  void setAdminUserModel(AdminUserModel? adminUserModel, {bool isNotify = true}) {
+    if(adminUserModel != null) {
+      _adminUserModel!.updateFromMap(adminUserModel.toMap());
+    }
+    else {
+      _adminUserModel = null;
+    }
+    if(isNotify) {
+      notifyListeners();
+    }
+  }
+}
