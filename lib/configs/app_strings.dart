38bb7b8d533bdcd3578836601dcd26604d51f474
bhavisha <bhavishaparmar21@gmail.com>
2022-09-08 15:59:46 +0530

    add diagnosis strings to app strings

--- lib/configs/app_strings.dart
+++ lib/configs/app_strings.dart
@@ -2,4 +2,28 @@ class AppStrings {
   static const String givenUserAlreadyExist = "User with given UserName already exist";
   static const String usernameOrPasswordIsEmpty = "UserName or password is empty";
   static const String login = "Login";
+
+
+  //diagnosis Page
+  static const String medicine = "Medicine";
+  static const String liquid = "Liquid";
+  static const String other = "Other";
+  static const String medicineType = "Type";
+  static const String advance = "Advance";
+  static const String name = "Name";
+  static const String quantity = "Quantity";
+  static const String enterQuantity = "Enter quantity";
+  static const String enterMedicineName = "Enter medicine name";
+  static const String instructions = "Instructions";
+  static const String enterExtraInstructions = "Enter extra instructions";
+  static const String time = "Time";
+  static const String morning = "Morning";
+  static const String afternoon = "Afternoon";
+  static const String evening = "Evening";
+  static const String beforeMeal = "Before meal";
+  static const String afterMeal = "After meal";
+  static const String dosage = "Dosage";
+  static const String enterDosage = "Enter dosage";
+  static const String cancel = "Cancel";
+  static const String done = "Done";
 }
