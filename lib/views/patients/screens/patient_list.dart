9d1f791a434f8da9192a3b69f1b900d85485519c
bhavisha <bhavishaparmar21@gmail.com>
2022-09-12 17:18:08 +0530

    create patient list cards

--- lib/views/patients/screens/patient_list.dart
+++ lib/views/patients/screens/patient_list.dart
@@ -1,3 +1,5 @@
+import 'package:doctor/packages/flux/flutx.dart';
+import 'package:doctor/views/common/components/get_key_value_widget.dart';
 import 'package:flutter/material.dart';
 
 class PatientList extends StatefulWidget {
@@ -21,13 +23,40 @@ class _PatientListState extends State<PatientList> {
         centerTitle: true,
         title: Text("Patients",style: themeData.textTheme.headline5,),
       ),*/
-      body: Container(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children:[]
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:List.generate(10, (index) => patientListCard())
+          ),
         ),
       ),
     );
   }
+
+
+  Widget patientListCard(){
+    return Container(
+      margin: EdgeInsets.only(top: 15),
+      child: FxCard(
+        shadow: FxShadow(elevation: 4),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                GetKeyValueWidget(keyString: "Patient Id:", value: "D012254"),
+                GetKeyValueWidget(keyString: "Patient Weight:", value: "50 kg"),
+              ],
+            ),
+            SizedBox(height: 10,),
+            Text("asdfjshegfndkxf kasgdfjsedfv kshifdhrg bskhfru idrhtgt swefe idheotg smbfie drhto awmnfeieshgir srngidrht",style: themeData.textTheme.bodyMedium,),
+          ],
+        ),
+      ),
+    );
+  }
+
+
 }
