dee2d652c9284a83849a8990449947ba347779da
bhavisha <bhavishaparmar21@gmail.com>
2022-09-12 16:27:02 +0530

    add visit bill details in form

--- lib/views/diagnosis/screens/diagnosis_form.dart
+++ lib/views/diagnosis/screens/diagnosis_form.dart
@@ -3,8 +3,12 @@ import 'package:doctor/views/common/components/common_text_form_field.dart';
 import 'package:doctor/views/common/components/profile_picture_circle.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../../../configs/app_strings.dart';
 import '../../../models/visit_model/prescription/prescription_model.dart';
+import '../../../packages/flux/widgets/button/button.dart';
+import '../../../packages/flux/widgets/text/text.dart';
 import '../../../utils/logger_service.dart';
 import 'get_medicine_dialog.dart';
 
@@ -20,8 +24,11 @@ class DiagnosisForm extends StatefulWidget {
 class _DiagnosisFormState extends State<DiagnosisForm> {
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _billFormKey = GlobalKey<FormState>();
   TextEditingController diagnosisTextController = TextEditingController();
   TextEditingController dietInstructionTextController = TextEditingController();
+  TextEditingController billTextController = TextEditingController();
+  TextEditingController discountTextController = TextEditingController();
   List<PrescriptionModel> prescriptionList=[];
 
   late ThemeData themeData;
@@ -44,28 +51,26 @@ class _DiagnosisFormState extends State<DiagnosisForm> {
       body: Container(
         padding: const EdgeInsets.all(10),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              getPatientInfo(),
-              const SizedBox(height: 10,),
-              Form(
-                key: _formKey,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    getTextFieldWithTitle(title: "Description",controller: diagnosisTextController),
-                    const SizedBox(height: 10,),
-                    /*getTextFieldWithTitle(title: "Diet instructions",controller: dietInstructionTextController),
-                    const SizedBox(height: 10,),*/
-                    getMedicineDetails(),
-                    const SizedBox(height: 10,),
-                    getPrescriptionTable(),
-                  ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                getPatientInfo(),
+                const SizedBox(height: 10,),
+                getTextFieldWithTitle(title: "Description",controller: diagnosisTextController),
+                const SizedBox(height: 10,),
+                /*getTextFieldWithTitle(title: "Diet instructions",controller: dietInstructionTextController),
+                const SizedBox(height: 10,),*/
+                getMedicineDetails(),
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-            ],
+                getVisitBill(),
+                getEndVisitButton(),
+              ],
+            ),
           ),
         ),
       ),
@@ -98,35 +103,26 @@ class _DiagnosisFormState extends State<DiagnosisForm> {
     );
   }
 
-  Widget getMedicineDetails() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  Widget getMedicineDetails(){
+    return Column(
       children: [
-        Text("Medicine Prescription",style: themeData.textTheme.headline6,),
-        InkWell(
-          onTap: () async{
-            List list = await showDialog(
-                barrierDismissible: false,
-                context: context,
-                builder: (context){
-                  return const GetMedicineDialog();
-            });
-            if (list[0] == true) {
-              prescriptionList.add(list[1]);
-              setState(() {});
+        getTitleWithAddButton(
+            title: AppStrings.medicinePrescription,
+            onTap: () async{
+                List list = await showDialog(
+                    barrierDismissible: false,
+                    context: context,
+                    builder: (context){
+                      return const GetMedicineDialog();
+                    });
+                if (list[0] == true) {
+                  prescriptionList.add(list[1]);
+                  setState(() {});
+                }
             }
-          },
-          child: Container(
-            padding: const EdgeInsets.all(5),
-            decoration: BoxDecoration(
-              color: themeData.primaryColor.withOpacity(0.2),
-              borderRadius: const BorderRadius.all(Radius.circular(5)),
             ),
-            child: Icon(FontAwesomeIcons.plus,size: 16,color: themeData.primaryColor,)
-            //Text("Add Medicine",style: TextStyle(fontSize: 14,color: themeData.primaryColor,fontWeight: FontWeight.w600),),
-
-          ),
-        ),
+        const SizedBox(height: 10,),
+        prescriptionList.length>0?getPrescriptionTable():SizedBox.shrink(),
       ],
     );
   }
@@ -210,7 +206,188 @@ class _DiagnosisFormState extends State<DiagnosisForm> {
     );
   }
 
+  Widget getVisitBill(){
+    return Column(
+      children: [
+        getTitleWithAddButton(
+            title: AppStrings.visitBill,
+          onTap: ()async{
+            List list = await showDialog(
+                barrierDismissible: false,
+                context: context,
+                builder: (context){
+                  return visitBillingDialog();
+                });
+              if (list[0] == true) {
+                setState(() {});
+              }
+          }
+        ),
+        const SizedBox(height: 10,),
+        billTextController.text.trim().isNotEmpty?getBillDetails():const SizedBox.shrink(),
+      ],
+    );
+  }
 
+  Widget getBillDetails(){
+    double discount  = (discountTextController.text.isNotEmpty?double.parse(discountTextController.text):0);
+    double total = double.parse(billTextController.text)-discount;
+    return Column(
+      children: [
+        getBillRows(title:AppStrings.consultancyFee,amount: billTextController.text),
+        SizedBox(height: 10,),
+        getBillRows(title:AppStrings.discountPrice,amount: "-$discount"),
+        SizedBox(height: 10,),
+        Divider(),
+        getBillRows(title:AppStrings.totalPrice,amount: "$total"),
+      ],
+    );
+  }
+
+  Widget getBillRows({required String title , required String amount}){
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title,style: themeData.textTheme.headline6,),
+        Text(amount,style: themeData.textTheme.headline6,),
+      ],
+    );
+  }
+
+  Widget getTitleWithAddButton({required String title,void Function()? onTap}) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(title,style: themeData.textTheme.headline6,),
+        InkWell(
+          onTap: onTap,
+          child: Container(
+              padding: const EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: themeData.primaryColor.withOpacity(0.2),
+                borderRadius: const BorderRadius.all(Radius.circular(5)),
+              ),
+              child: Icon(FontAwesomeIcons.plus,size: 16,color: themeData.primaryColor,)
+            //Text("Add Medicine",style: TextStyle(fontSize: 14,color: themeData.primaryColor,fontWeight: FontWeight.w600),),
+
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getEndVisitButton(){
+    return FxButton.small(
+      block: true,
+      borderRadiusAll: 4,
+      buttonType: FxButtonType.outlined,
+      splashColor: themeData.colorScheme.primary.withAlpha(60),
+      borderColor: themeData.primaryColor,
+      onPressed: () {
+      },
+      elevation: 0,
+      child: FxText.bodyMedium(
+      AppStrings.endVisit,
+      color: themeData.primaryColor,
+      ),
+    );
+  }
+
+  Widget visitBillingDialog(){
+    return Dialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: SingleChildScrollView(
+        child: Form(
+          key: _billFormKey,
+          child: Container(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              children: [
+                getTextEditingFieldWithTitle(
+                  title: AppStrings.consultancyFee,
+                  controller: billTextController,
+                  hintText: AppStrings.consultancyFee,
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return AppStrings.enterFee;
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10,),
+                getTextEditingFieldWithTitle(title: AppStrings.discountPrice,controller: discountTextController,hintText: AppStrings.discountPrice),
+                const SizedBox(height: 10,),
+                getLastButtons(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getTextEditingFieldWithTitle({required String title,required TextEditingController controller , String? hintText, String? Function(String?)? validator}){
+    return Row(
+      children: [
+        Text(title,style: themeData.textTheme.headline6,),
+        const SizedBox(width: 15,),
+        Expanded(
+            child: CommonTextFormField(
+              controller: controller,
+              keyboardType: TextInputType.number,
+              inputFormatter: [
+                FilteringTextInputFormatter.digitsOnly
+              ],
+              hintText: hintText,
+              validator: validator,
+            ),
+        ),
+
+      ],
+    );
+  }
+
+  Widget getLastButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        Expanded(
+          child: FxButton.small(
+            borderRadiusAll: 4,
+            buttonType: FxButtonType.outlined,
+            splashColor: themeData.colorScheme.primary.withAlpha(60),
+            borderColor: themeData.primaryColor,
+            onPressed: () {
+              Navigator.pop(context,[false]);
+            },
+            elevation: 0,
+            child: FxText.bodyMedium(
+              AppStrings.cancel,
+              color: themeData.primaryColor,
+            ),
+          ),
+        ),
+        SizedBox(width: 10,),
+        Expanded(
+          child: FxButton.small(
+            borderRadiusAll: 4,
+            backgroundColor: themeData.primaryColor,
+            onPressed: () {
+              if(_billFormKey.currentState!.validate()){
+                Navigator.pop(context,[true]);
+              }
+            },
+            elevation: 0,
+            splashColor: themeData.colorScheme.onPrimary.withAlpha(60),
+            child: FxText.bodyMedium(
+              AppStrings.done,
+              color: themeData.colorScheme.onPrimary,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
 }
 
 
